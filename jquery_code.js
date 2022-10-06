527f76fc0f5f839ee941ac7c146bc8ccd1300ab1
reocat <111279389+reocat@users.noreply.github.com>
2022-10-07 00:00:21 +0300

    Bring audio feature back

--- jquery_code.js
+++ jquery_code.js
@@ -4,3 +4,24 @@ func =function(){ $.getJSON('https://aws.random.cat/meow')
     $('#cat_pic').attr('src',data.file)
 });}
 
+$(document).ready(function() {
+
+  $('#selection').on('change', function() {
+    change($(this).val());
+  });
+
+});
+
+
+function change(sourceUrl) {
+  var audio = document.getElementById("player");
+  var source = document.getElementById("mp3_src");
+
+  audio.pause();
+
+  if (sourceUrl) {
+    source.src = sourceUrl;
+    audio.load();
+    audio.play();
+  }
+}
