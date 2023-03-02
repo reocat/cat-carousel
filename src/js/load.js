cccc387c2b9999051e0142c8ded903cf2a46f66e
Charlie Root <root@nyan.lan>
2023-03-02 21:03:53 +0300

    another try to make somethin' broken

--- src/js/load.js
+++ src/js/load.js
@@ -1,3 +1,7 @@
+//CSS
+require('../css/style.css');
+import '../css/style.css';
+//JS
 window.$ = require('jquery');
 require('./vanilla.js');
 require('./jquery_code.js');
--- webpack.config.js
+++ webpack.config.js
@@ -5,4 +5,23 @@ module.exports = {
 	path: `${__dirname}/dist`,
 	filename: 'bundle.js',
   },
+  module: {
+	rules: [
+	  {
+		test: /\.css$/,
+		use: [
+		  'style-loader',
+		  'css-loader',
+		],
+	  },
+       {
+         test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
+         use: ['file-loader']
+       },
+       { 
+	 test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
+	 use: ['url-loader?limit=100000'] 
+	}
+	],
+  },
 };
