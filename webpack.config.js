fdd525c07543f307afb7f317024b6abd42f9b765
maple <root@nyan.lan>
2023-03-02 22:02:22 +0300

    let's make everything broken

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const path = require('path');
+var WebpackObfuscator = require('webpack-obfuscator');
+
 module.exports = {
   entry: './src/js/load.js',
   mode: 'development',
@@ -5,6 +8,10 @@ module.exports = {
 	path: `${__dirname}/dist`,
 	filename: 'bundle.js',
   },
+
+  plugins: [
+        new WebpackObfuscator({rotateStringArray: true, reservedStrings: [ '\s*' ]}, [])
+    ],
   module: {
 	rules: [
 	  {
@@ -21,6 +28,16 @@ module.exports = {
        { 
 	 test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
 	 use: ['url-loader?limit=100000'] 
+	},
+	{
+	  enforce: 'post',
+	  use: {
+	        loader: WebpackObfuscator.loader,
+        	options: {
+            		rotateStringArray: true,
+            		reservedStrings: [ '\s*' ]
+        	}
+    	  }
 	}
 	],
   },
