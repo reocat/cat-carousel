b68c2da741f1236dc919c4c7ca6f776dfe934bbc
reocat <111279389+reocat@users.noreply.github.com>
2023-05-02 11:01:36 +0300

    test commit

--- src/js/vanilla.js
+++ src/js/vanilla.js
@@ -1,78 +1,78 @@
-// Declare constants with const
-const duration = '0.5s';
-const animation = 'animate__tada';
+// cache frequently used DOM elements
 const btnDeathMode = document.querySelector('.btn-container');
 const buttonRefresh = document.querySelector('.btn-refr');
-const slides = document.querySelectorAll('.slide');
-const nextSlide = document.querySelector('.btn-next');
-const prevSlide = document.querySelector('.btn-prev');
-const btnContainer = document.querySelector('.btn-container');
-const body = document.querySelector('body');
-const musicOn = document.querySelector('.musicOn');
-const h1 = document.querySelector('h1');
+const slides = document.querySelectorAll(".slide");
+const nextSlide = document.querySelector(".btn-next");
+const prevSlide = document.querySelector(".btn-prev");
 
-// Add event listeners
+// declare variables
+const animation = 'animate__tada';
+const duration = '0.5s';
+let curSlide = 0;
+const maxSlide = slides.length - 1;
+
+// add event listener to toggle class and change css property
 btnDeathMode.addEventListener('click', function() {
   btnDeathMode.classList.add(animation);
   btnDeathMode.style.animationDuration = duration;
   setTimeout(function() {
     btnDeathMode.classList.remove(animation);
   }, 550);
+  document.querySelector('body').classList.toggle('narc-bg');
+  document.querySelector('.musicOn').classList.toggle('show');
+  document.querySelector('h1').classList.toggle('nn');
 });
 
-btnContainer.addEventListener('click', function() {
-  body.classList.toggle('narc-bg');
-  musicOn.classList.toggle('show');
-  h1.classList.toggle('nn');
-});
+// add event listener to fetch cat images and refresh slides
+buttonRefresh.addEventListener('click', fetchCatImages);
 
-
-console.log(buttonRefresh);
-buttonRefresh.addEventListener('click', function() {
-var list = document.querySelectorAll('img');
-for (var i = 0; i < list.length; i++) {
-    list[i].src = list[i].src + ' ';
-    fetchCatImages();
-}
-});
-
-nextSlide.addEventListener('click', () => changeSlide(1));
-prevSlide.addEventListener('click', () => changeSlide(-1));
-
-// Initialize slide positions
+// loop through slides and set each slides translateX
 slides.forEach((slide, indx) => {
   slide.style.transform = `translateX(${indx * 100}%)`;
 });
 
-let curSlide = 0;
-const maxSlide = slides.length - 1;
-
-function changeSlide(delta) {
-  curSlide = (curSlide + delta + slides.length) % slides.length;
+// add event listener and navigation functionality for next slide button
+nextSlide.addEventListener("click", function() {
+  if (curSlide === maxSlide) {
+    fetchCatImages();
+    curSlide = 0;  
+  } else {
+    curSlide++;
+  }
   slides.forEach((slide, indx) => {
     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
   });
-}
+});
 
-async function fetchCatImages() {
-  try {
-    const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=10');
-    const data = await response.json();
-    const [url1, url2, url3] = data.map(image => image.url);
-    document.getElementById('slide1').src = url1;
-    document.getElementById('slide2').src = url2;
-    document.getElementById('slide3').src = url3;
-  } catch (error) {
-    console.error(error);
+// add event listener and navigation functionality for previous slide button
+prevSlide.addEventListener("click", function() {
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;  
   }
-}
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
 
-// Check URL parameters
+// check if URL param 'free=1' is present and redirect
 if (window.location.search.includes('free=1')) {
-  if (confirm('Don\'t forget to use VPN')) {
+  if (confirm("Don't forget to use VPN") == true) {
     window.location.href = 'https://free.navalny.com';
   }
 }
 
-// Call fetchCatImages on page load
-fetchCatImages();
+// fetch cat images using async/await
+async function fetchCatImages() {
+  try {
+    const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=10');
+    const data = await response.json();
+    const urls = data.map(image => image.url);
+    document.getElementById('slide1').src = urls[0];
+    document.getElementById('slide2').src = urls[1];
+    document.getElementById('slide3').src = urls[2];
+  } catch (error) {
+    console.error(error);
+  }
+}
