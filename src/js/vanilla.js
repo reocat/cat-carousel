cf26a8978b8e523f75dbf03e9e0bca306379990e
reocat <111279389+reocat@users.noreply.github.com>
2023-05-02 16:10:14 +0300

    Update vanilla.js

--- src/js/vanilla.js
+++ src/js/vanilla.js
@@ -63,11 +63,31 @@ if (window.location.search.includes('free=1')) {
   }
 }
 
+function getCookie(name) {
+    // Split cookie string and get all individual name=value pairs in an array
+    var cookieArr = document.cookie.split(";");
+    
+    // Loop through the array elements
+    for(var i = 0; i < cookieArr.length; i++) {
+        var cookiePair = cookieArr[i].split("=");
+        
+        /* Removing whitespace at the beginning of the cookie name
+        and compare it with the given string */
+        if(name == cookiePair[0].trim()) {
+            // Decode the cookie value and return
+            return decodeURIComponent(cookiePair[1]);
+        }
+    }
+    
+    // Return null if not found
+    return null;
+}
+
 // fetch cat images using async/await
 async function fetchCatImages() {
   let apiUrl = 'https://api.thecatapi.com/v1/images/search?limit=10';
   try {
-      if (window.location.search.includes('shibe=1')) {
+      if (getCookie(shibe) = '1')) {
         apiUrl = 'https://shibe.online/api/cats?count=3';
         fetch(apiUrl)
         .then(response => response.json())
