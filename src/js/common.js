46ca7986a5f8d60f071b725ef84fa0dc3b3e918f
unknown <debug@ITcomp01.kvantum.local>
2023-05-19 18:49:32 +0300

    Revert "test commit 3"
    This reverts commit 0d7c7e8beb399e2e5131ffc86660fe2d7567f560.

--- src/js/common.js
+++ src/js/common.js
@@ -1,15 +1,1 @@
-async function getCookie(name) { // function to get the value of a cookie
-  let cookieValue = "";
-  const cookies = document.cookie.split(";"); // split all cookies into an array
 
-  cookies.forEach((cookie) => { // loop through each cookie
-    let [cookieName, cookieVal] = cookie.split("="); // get cookie's name and value
-    cookieName = cookieName.trim(); // remove leading/trailing whitespaces
-
-    if (cookieName === name) { // if cookie's name is the required one
-      cookieValue = cookieVal; // save its value
-    }
-  });
-
-  return cookieValue; // return cookie's value
-}
