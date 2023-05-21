77bacda8d2b185b632474a7fd435033d521784de
reocat <111279389+reocat@users.noreply.github.com>
2023-05-21 16:53:51 +0300

    Create common.js

---
+++ src/js/common.js
@@ -0,0 +1,19 @@
+      function getCookie(cookieName) {
+        var name = cookieName + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var cookieArray = decodedCookie.split(';');
+
+        for (var i = 0; i < cookieArray.length; i++) {
+          var cookie = cookieArray[i];
+
+          while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+          }
+
+          if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
+          }
+        }
+
+        return "";
+      }
