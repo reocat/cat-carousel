7d329b3b0491f56b8eaef59a4009f93c5094de01
reocat <111279389+reocat@users.noreply.github.com>
2023-05-21 16:55:07 +0300

    Update vanilla.js

--- src/js/vanilla.js
+++ src/js/vanilla.js
@@ -71,21 +71,6 @@ if (window.location.search.includes('free=1')) {
   }
 }
 
-function getCookie(name) { // function to get the value of a cookie
-  let cookieValue = "";
-  const cookies = document.cookie.split(";"); // split all cookies into an array
-
-  cookies.forEach((cookie) => { // loop through each cookie
-    let [cookieName, cookieVal] = cookie.split("="); // get cookie's name and value
-    cookieName = cookieName.trim(); // remove leading/trailing whitespaces
-
-    if (cookieName === name) { // if cookie's name is the required one
-      cookieValue = cookieVal; // save its value
-    }
-  });
-
-  return cookieValue; // return cookie's value
-}
 async function CatAPI() {
        let apiUrl = 'https://api.thecatapi.com/v1/images/search?limit=10';
        const response = await fetch(apiUrl);
