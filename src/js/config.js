ae77cc03814ecc5b1e9bbb684639df374f787715
reocat <111279389+reocat@users.noreply.github.com>
2023-06-15 09:38:03 +0300

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,1 +1,40 @@
+function popup() {
+        alert("Configuration applied, nya!");
+        window.location.href = window.location = '/';
+}
 
+function setCookie() {
+        var apiSelect = document.getElementById("api-select");
+        var apiValue = apiSelect.options[apiSelect.selectedIndex].value;
+        
+        var colorSelect = document.getElementById("color-select");
+        var colorValue = colorSelect.value;
+
+        if (apiValue === "catapi") {
+          document.cookie = "CurAPI=catapi";
+          document.cookie = "color=" + encodeURIComponent(colorValue);
+          popup();
+        } else if (apiValue === "shibe") {
+          document.cookie = "CurAPI=shibe";
+          document.cookie = "color=" + encodeURIComponent(colorValue);
+          popup();
+        }
+      }
+
+      // Set the default value of the dropdown list based on the "CurAPI" cookie
+      function setDefaultAPIValue() {
+        var apiSelect = document.getElementById("api-select");
+        var curAPI = getCookie("CurAPI");
+
+        if (curAPI) {
+          for (var i = 0; i < apiSelect.options.length; i++) {
+            if (apiSelect.options[i].value === curAPI) {
+              apiSelect.selectedIndex = i;
+              break;
+            }
+          }
+        }
+      }
+
+// Call the setDefaultAPIValue function when the page loads
+window.onload = setDefaultAPIValue;
