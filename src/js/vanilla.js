bf44506a66b901c93af6dbeec03ceaafbcaf4771
reocat <111279389+reocat@users.noreply.github.com>
2023-06-15 10:22:38 +0300

    Update vanilla.js

--- src/js/vanilla.js
+++ src/js/vanilla.js
@@ -1,5 +1,13 @@
 // Get the value of the "color" cookie
 const colorValue = getCookie('color');
+const dir = require.context('./api', true, /\.js$/);
+
+const modules = {};
+   dir.keys().forEach(key => {
+     const module = dir(key);
+     modules[key.replace(/\.\/|\.js/g, '')] = module;
+});
+   
 
 // Apply the color value as the background color globally
 if (colorValue) {
@@ -71,15 +79,8 @@ if (window.location.search.includes('free=1')) {
   }
 }
 
-async function CatAPI() {
-       let apiUrl = 'https://api.thecatapi.com/v1/images/search?limit=10';
-       const response = await fetch(apiUrl);
-       const data = await response.json();
-       const urls = data.map(image => image.url);
-       document.getElementById('slide1').src = urls[0];
-       document.getElementById('slide2').src = urls[1];
-       document.getElementById('slide3').src = urls[2];
-}
+  
+
 
 async function ShibeAPI() {
         let apiUrl = 'https://shibe.online/api/cats?count=3';
