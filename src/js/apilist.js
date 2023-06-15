e97f4d8722599487bf052a31186c280c7d4415a8
reocat <111279389+reocat@users.noreply.github.com>
2023-06-15 10:59:50 +0300

    Update apilist.js

--- src/js/apilist.js
+++ src/js/apilist.js
@@ -1,4 +1,4 @@
-async function ShibeAPI() {
+export async function ShibeAPI() {
         let apiUrl = 'https://shibe.online/api/cats?count=3';
         fetch(apiUrl)
         .then(response => response.json())
@@ -8,4 +8,13 @@ async function ShibeAPI() {
            document.getElementById("slide2").src = data[1];
            document.getElementById("slide3").src = data[2];
 })
+}
+export async function CatAPI() {
+       let apiUrl = 'https://api.thecatapi.com/v1/images/search?limit=10';
+       const response = await fetch(apiUrl);
+       const data = await response.json();
+       const urls = data.map(image => image.url);
+       document.getElementById('slide1').src = urls[0];
+       document.getElementById('slide2').src = urls[1];
+       document.getElementById('slide3').src = urls[2];
 }
