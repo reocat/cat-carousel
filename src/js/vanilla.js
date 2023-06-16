c93410f0fbf5faac9f57ddbcb393c627f128df0f
reocat <111279389+reocat@users.noreply.github.com>
2023-06-16 19:11:50 +0300

    Update vanilla.js

--- src/js/vanilla.js
+++ src/js/vanilla.js
@@ -96,3 +96,8 @@ async function fetchCatImages() {
 }
 
 fetchCatImages();
+//add error listener
+window.onerror = function(msg, url, linenumber) {
+    alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
+    return true;
+}
