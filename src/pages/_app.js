de4865d33966f9e60ceca8a5970342d60e0550c1
reocat <ng.ct_ml@tuta.io>
2023-07-09 12:21:30 +0300

    add something

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,114 @@
-import { useState, useEffect } from 'react';
-import '../styles/global.css';
-import '../styles/fonts.css';
+import { useState, useEffect, useRef } from 'react';
 import { Tooltip } from 'react-tooltip';
 import TextTransition, { presets } from 'react-text-transition';
+import ReactPlayer from 'react-player';
+
+// Import files
+import useRainbow from './useRainbow.hook';
+import '../styles/global.css';
+import '../styles/fonts.css';
+
+const MagicRainbowButton = ({ children, intervalDelay = 1000 }) => {
+  const colors = useRainbow({ intervalDelay });
+  const colorKeys = Object.keys(colors);
+  const transitionDelay = 200;
+  const buttonElemRef = useRef(null);
+  const [selectedMusic, setSelectedMusic] = useState('');
+  const [showDropdown, setShowDropdown] = useState(false); // New state variable
+
+  const handleMusicSelection = (event) => {
+    setSelectedMusic(event.target.value);
+  };
+
+  useEffect(() => {
+    const buttonElem = buttonElemRef.current;
+
+    function handleClick() {
+      if (document.body.classList.contains('nyan')) {
+        document.body.classList.remove('nyan');
+      } else {
+        document.body.classList.add('nyan');
+      }
+
+      setShowDropdown(!showDropdown); // Toggle dropdown visibility
+    }
+
+    buttonElem.addEventListener('click', handleClick);
+
+    return () => {
+      buttonElem.removeEventListener('click', handleClick);
+    };
+  }, [showDropdown]); // Update effect dependency
+
+  return (
+    <div>
+      <button
+        id="rainbow-button"
+        ref={buttonElemRef}
+        style={{
+          fontFamily: '04b03',
+          padding: '5px 30px',
+          border: 'none',
+          borderRadius: '10px',
+          fontSize: '14px',
+          color: '#fff',
+          cursor: 'pointer',
+          ...colors,
+          transition: `
+            ${colorKeys[0]} ${transitionDelay}ms linear,
+            ${colorKeys[1]} ${transitionDelay}ms linear,
+            ${colorKeys[2]} ${transitionDelay}ms linear
+          `,
+          background: `
+            radial-gradient(
+              circle at top left,
+              var(${colorKeys[2]}),
+              var(${colorKeys[1]}),
+              var(${colorKeys[0]})
+            )
+          `,
+        }}
+      >
+        {children}
+      </button>
+      {showDropdown && (
+        <select value={selectedMusic} onChange={handleMusicSelection}>
+          <option value="">Select Music</option>
+          <option value="lo-fi">Lo-fi</option>
+          <option value="nyan-cat">Nyan Cat Soundtrack</option>
+          <option value="rain">Rain</option>
+        </select>
+      )}
+      {selectedMusic === 'lo-fi' && (
+        <ReactPlayer
+          url="https://streams.fluxfm.de/Chillhop/mp3-128/streams.fluxfm.de"
+          playing={true}
+          loop={true}
+          width={0}
+          height={0}
+        />
+      )}
+      {selectedMusic === 'nyan-cat' && (
+        <ReactPlayer
+          url="https://www.nyan.cat/music/original.mp3"
+          playing={true}
+          loop={true}
+          width={0}
+          height={0}
+        />
+      )}
+      {selectedMusic === 'rain' && (
+        <ReactPlayer
+          url="https://stream.willstare.com:8850/;?type=http&nocache=9305"
+          playing={true}
+          loop={true}
+          width={0}
+          height={0}
+        />
+      )}
+    </div>
+  );
+};
 
 const ImageCarousel = () => {
   const [images, setImages] = useState([]);
@@ -46,6 +152,9 @@ const ImageCarousel = () => {
 
   return (
     <div className="carousel">
+      <MagicRainbowButton intervalDelay={1500}>
+        toggle death mode
+      </MagicRainbowButton>
       {images.length > 0 && (
         <div className="image-container">
           <img
@@ -88,9 +197,9 @@ const HomePage = () => {
       </div>
       <a id="tooltip">Authors</a>
       <Tooltip anchorSelect="#tooltip" clickable>
-        <span class="tooltiptext">
-          Made by <a class="links" href="https://github.com/reocat">reocat</a> and{' '}
-          <a class="links" href="https://github.com/L1ttleWizard">L1ttleWizard</a>
+        <span className="tooltiptext">
+          Made by <a className="links" href="https://github.com/reocat">reocat</a> and{' '}
+          <a className="links" href="https://github.com/L1ttleWizard">L1ttleWizard</a>
         </span>
       </Tooltip>
     </div>
