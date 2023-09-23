14f9f0bdf4c91b748ef4bf723187341ec2a419fd
reocat <111279389+reocat@users.noreply.github.com>
2023-09-23 20:20:47 +0300

    Update animality.js
    api endpoints update

--- src/app/api/animality.js
+++ src/app/api/animality.js
@@ -1,13 +1,13 @@
 export const fetchAnimalityApiImages = async () => {
     try {
       const numImages = 10; // Number of images to fetch per request
-      const apiUrl = 'https://neko.sunflowers-warp.workers.dev/img/cat'; // Proxy for Animality, original link blocked by browser policies
+      const apiUrl = 'https://neko.sunflowers-warp.workers.dev/all/cat'; // Proxy for Animality, original link blocked by browser policies
       const images = [];
   
       for (let i = 0; i < numImages; i++) {
         const response = await fetch(apiUrl);
         const data = await response.json();
-        images.push(data.link);
+        images.push(data.image);
       }
   
       return images;
