e1656f1d8b21f78589d9829b064152e7c7e67f5f
reocat <111279389+reocat@users.noreply.github.com>
2023-10-23 09:54:14 +0300

    Create purrbot.js

---
+++ src/app/api/purrbot.js
@@ -0,0 +1,19 @@
+export const fetchPurrBotApiImages = async () => {
+    try {
+      const numImages = 10; // Number of images to fetch per request
+      const apiUrl = 'https://purrbot.site/api/img/sfw/' + plushie +'/img';
+      const images = [];
+  
+      for (let i = 0; i < numImages; i++) {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        images.push(data.url);
+      }
+  
+      return images;
+    } catch (error) {
+      console.error('Error fetching neko images:', error);
+      throw error;
+    }
+  };
+  
