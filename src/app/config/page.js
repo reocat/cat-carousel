64a1c2c6602a25468bafaf0083291e1441638954
reocat <111279389+reocat@users.noreply.github.com>
2023-12-18 20:20:48 +0300

    chat, j'ai pété

--- src/app/config/page.js
+++ src/app/config/page.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import Cookies from "js-cookie";
 import "../styles/fonts.css";
 import "../styles/index.css";
+import "./home.css"; // Create a new CSS file for Home component styles
 
 export default function Home() {
   const [selectedValue, setSelectedValue] = useState("");
@@ -40,31 +41,33 @@ export default function Home() {
   };
 
   return (
-    <div>
+    <div className="home-container">
       <h1>Page Configurator</h1>
-      <label htmlFor="apiSelect">Select API:</label>
-      <br />
-      <select
-        id="apiSelect"
-        value={selectedValue}
-        onChange={handleSelectChange}
-      >
-        <option value="">-- Select --</option>
-        <option value="catapi">The Cat API</option>
-        <option value="shibe">Shibe API</option>
-        <option value="animality">Animality API</option>
-      </select>
-      <br />
-      <label htmlFor="colorPicker">Select Color:</label>
-      <br />
-      <input
-        type="color"
-        id="colorPicker"
-        value={colorValue}
-        onChange={handleColorChange}
-      />
-      <br />
-      <button onClick={handleSaveSettings}>Save Settings</button>
+      <div className="form-group">
+        <label htmlFor="apiSelect">Select API:</label>
+        <select
+          id="apiSelect"
+          value={selectedValue}
+          onChange={handleSelectChange}
+        >
+          <option value="">-- Select --</option>
+          <option value="catapi">The Cat API</option>
+          <option value="shibe">Shibe API</option>
+          <option value="animality">Animality API</option>
+        </select>
+      </div>
+      <div className="form-group">
+        <label htmlFor="colorPicker">Select Color:</label>
+        <input
+          type="color"
+          id="colorPicker"
+          value={colorValue}
+          onChange={handleColorChange}
+        />
+      </div>
+      <button className="save-button" onClick={handleSaveSettings}>
+        Save Settings
+      </button>
     </div>
   );
 }
