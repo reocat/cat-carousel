13b0cb5ca4e07a07dbf2f804595fece8f3f1bf4f
reocat <ng.ct_ml@tuta.io>
2023-12-22 00:30:21 +0300

    code format

--- src/app/redux/reducers.js
+++ src/app/redux/reducers.js
@@ -1,27 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    active: false,
+  active: false,
 };
 export const switchSlice = createSlice({
-    name: "switch",
-    initialState,
-    reducers: {
-        on: (state) => {
-            state.active = true;
-        },
-        off: (state) => {
-            state.active = false;
-        },
-        toggle: (state) => {
-            const current = state.active;
-            if (current === true) {
-                state.active = false;
-            } else {
-                state.active = true;
-            }
-        },
+  name: "switch",
+  initialState,
+  reducers: {
+    on: (state) => {
+      state.active = true;
     },
+    off: (state) => {
+      state.active = false;
+    },
+    toggle: (state) => {
+      const current = state.active;
+      if (current === true) {
+        state.active = false;
+      } else {
+        state.active = true;
+      }
+    },
+  },
 });
 export const hellStateReducer = switchSlice.reducer;
-export const {on,off,toggle} = switchSlice.actions;
+export const { on, off, toggle } = switchSlice.actions;
--- src/app/redux/storeProvider.jsx
+++ src/app/redux/storeProvider.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { Provider } from "react-redux";
-import { store } from './store'
+import { store } from "./store";
 export default function StoreProvider({ children }) {
-    return <Provider store={store}>{children}</Provider>;
+  return <Provider store={store}>{children}</Provider>;
 }
