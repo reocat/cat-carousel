62207de700df3c6f94d5366a9b654d9141e3d338
reocat <ng.ct_ml@tuta.io>
2023-12-22 18:57:58 +0000

    Rework Nekos API

---
+++ src/app/redux/api/nekoapi.js
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const nekoapi = createApi({
+  reducerPath: 'nekoapi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://nekos.life/api/v2/img/neko' }),
+  endpoints: (builder) => ({
+    getNekoApi: builder.query({
+      query: (numImages) => `?limit=${numImages}`,
+    }),
+  }),
+});
+
+export const { useGetNekoApiQuery } = nekoapi;
