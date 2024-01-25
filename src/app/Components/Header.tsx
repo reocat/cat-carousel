4a547f425175b180920ab6d3f12e2319eb252c3f
l1ttlewizard <andrwyandreev2005@gmail.com>
2024-01-25 03:24:07 +0300

    started working on the new apis

---
+++ src/app/Components/Header.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import { useSelector } from 'react-redux';
+import { doLogout, selectUserIsLogged } from '../redux/reducers';
+import { useRouter } from 'next/navigation';
+import { useDispatch } from 'react-redux';
+import { Button } from '@mui/material';
+
+export default function Header() {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const auth = useSelector(selectUserIsLogged)
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar className='bg-gray-300 bg-opacity-65'>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Cat Carousel
+          </Typography> 
+          { auth?  (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={()=>{ setAnchorEl(null); router.push('/config')}}>Config</MenuItem>
+                <MenuItem onClick={()=>{ dispatch(doLogout());}}>Logout</MenuItem>
+              </Menu>
+            </div>
+          ):(<Button color="inherit" onClick={()=>{router.push('/login')}}>Login</Button>)}
+          
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+}
