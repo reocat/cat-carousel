b3c9d3c5428a7bde09f7652ffbe6f55090d0c8a0
reocat <ng.ct_ml@tuta.io>
2024-07-17 16:00:59 +0300

    Bring back custom 404 error

---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function NotFound() {
+  const containerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh", // You can adjust this value if you want a different height
+  };
+
+  const imageStyle = {
+    objectFit: "fill",
+  };
+
+  return (
+    <div style={containerStyle}>
+      <img src="https://http.cat/404" alt="404 - Page Not Found" style={imageStyle} />
+    </div>
+  )
+}
+
+export default NotFound
